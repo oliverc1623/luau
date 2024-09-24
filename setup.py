8d12d318c7f77f03722d65c58f1d141d9e59dbec
Oliver Chang <oliverc1622@gmail.com>
2024-09-24 09:04:37 -0700

    setup.py update

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
+
+
 setup(
-    name='luau',
+    name="luau",
     version=0.1,
     packages=find_packages(),
 )
