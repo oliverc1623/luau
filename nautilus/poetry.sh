88ac121c548423ed5cb0e41a9972b7205a47bed2
Oliver Chang <oliverc1622@gmail.com>
2024-09-24 15:40:28 -0700

    luau conda env

--- nautilus/poetry.sh
+++ nautilus/poetry.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-micromamba create -n poetry python=3.11 -c conda-forge
+micromamba create -n poetry python=3.11 -c conda-forge -y
 micromamba run -n poetry pipx ensurepath
 micromamba run -n poetry pipx install poetry
+
+micromamba create -n luau python=3.11 -c conda-forge -y
