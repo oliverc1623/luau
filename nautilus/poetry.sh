dc321a91a44755138ecd41d213cdd430b2c75c87
Oliver Chang <oliverc1622@gmail.com>
2024-09-25 12:52:01 -0700

    doing it all in one

--- nautilus/poetry.sh
+++ nautilus/poetry.sh
@@ -5,3 +5,9 @@ micromamba run -n poetry pipx ensurepath
 micromamba run -n poetry pipx install poetry
 
 micromamba create -n luau python=3.11 -c conda-forge -y
+micromamba activate luau
+git clone https://github.com/oliverc1623/luau.git
+cd luau
+poetry install
+poetry run inv setup
+python luau/train.py --log_dir ~/../pvcvolume --model_dir ~/../pvcvolume --num_experiments 4
