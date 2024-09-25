249d41ca514d9109aad329a797cb19dd40f27837
Oliver Chang <oliverc1622@gmail.com>
2024-09-25 13:59:44 -0700

    plz

--- nautilus/poetry.sh
+++ nautilus/poetry.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+eval "$(micromamba shell hook --shell=bash)"
 micromamba create -n poetry python=3.11 -c conda-forge -y
 micromamba run -n poetry pipx ensurepath
+export PATH="/root/.local/bin:$PATH"
 micromamba run -n poetry pipx install poetry
 
+eval "$(micromamba shell hook --shell=bash)"
 micromamba create -n luau python=3.11 -c conda-forge -y
+export PATH="/root/.local/bin:$PATH"
 micromamba run -n luau poetry install
 micromamba run -n luau poetry run inv setup
 micromamba run -n luau python luau/train.py --log_dir ~/../pvcvolume --model_dir ~/../pvcvolume --num_experiments 4
