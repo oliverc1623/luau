230439a4666ea25b0f20e724a26ed4fc7f60a3fa
Oliver Chang <oliverc1622@gmail.com>
2024-09-25 16:09:43 -0700

    specify pvcvolume

--- nautilus/poetry.sh
+++ nautilus/poetry.sh
@@ -11,5 +11,5 @@ micromamba create -n luau python=3.11 -c conda-forge -y
 export PATH="/root/.local/bin:$PATH"
 micromamba run -n luau poetry install
 micromamba run -n luau poetry run inv setup
-micromamba run -n luau tensorboard --logdir=./PPO_logs &
+micromamba run -n luau tensorboard --logdir=~/../pvcvolume/PPO_logs &
 micromamba run -n luau python luau/train.py --log_dir ~/../pvcvolume --model_dir ~/../pvcvolume --num_experiments 4
