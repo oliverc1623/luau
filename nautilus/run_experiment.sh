86547e98ef9545e5535f9474b44be8101835c2c3
Oliver Chang <oliverc1622@gmail.com>
2024-09-25 23:59:28 -0700

    train ppo locked

---
+++ nautilus/run_experiment.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+eval "$(micromamba shell hook --shell=bash)"
+micromamba create -n poetry python=3.11 -c conda-forge -y
+micromamba run -n poetry pipx ensurepath
+export PATH="/root/.local/bin:$PATH"
+micromamba run -n poetry pipx install poetry
+
+eval "$(micromamba shell hook --shell=bash)"
+micromamba create -n luau python=3.11 -c conda-forge -y
+export PATH="/root/.local/bin:$PATH"
+micromamba run -n luau poetry install
+micromamba run -n luau poetry run inv setup
+micromamba run -n luau tensorboard --logdir=~/../pvcvolume/PPO_logs &
+micromamba run -n luau python luau/train.py --config_path ./hyperparams/ppo-iaa-env-locked-config.yaml --log_dir ~/../pvcvolume --model_dir ~/../pvcvolume --num_experiments 4
