9a0607318907a44d4220ddee9649e9643ceecd5f
Oliver Chang <oliverc1622@gmail.com>
2025-01-23 00:03:40 +0000

    remove old imports

--- luau/inference.py
+++ luau/inference.py
@@ -7,8 +7,7 @@ import numpy as np
 import torch
 from minigrid.wrappers import ImgObsWrapper
 
-from luau.iaa_env import SmallIntrospectiveEnv
-from luau.no_frills_ppo import Agent
+from luau.ppo import Agent
 
 
 root_path = Path(__file__).resolve().parent.parent
@@ -41,7 +40,7 @@ def make_env(subenv_seed: int) -> gym.Env:
     """Create the environment."""
 
     def _init() -> gym.Env:
-        env = SmallIntrospectiveEnv(size=6, locked=door_locked, render_mode="rgb_array")
+        env = gym.make("FourRoomDoorKeyLocked-v0", render_mode="rgb_array")
         env = gym.wrappers.RecordEpisodeStatistics(env)
         env = ImgObsWrapper(env)
         env.reset(seed=subenv_seed)
