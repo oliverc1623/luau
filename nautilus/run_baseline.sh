930f1e41757739d55fdf30ad75cf65261be4cf30
Oliver Chang <oliverc1622@gmail.com>
2025-01-23 18:38:30 +0000

    baseline script

---
+++ nautilus/run_baseline.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Define the number of runs
+num_runs=5
+
+# Define the parameters for each run
+declare -a seeds=("11" "47" "97" "457" "809")  # Example seeds
+
+# Loop through the runs and execute the Python script with different arguments
+for ((run=1; run<=num_runs; run++)); do
+  seed=${seeds[$((run-1))]}  # Get the seed for this run
+  echo "Running experiment $run with seed $seed"
+
+  python luau/ppo.py \
+    --exp-name "PPO_Teacher_Source_Run${run}" \
+    --gym-id "MiniGrid-Empty-8x8-v0-v0" \
+    --learning-rate 0.0005 \
+    --total-timesteps 500_000 \
+    --num-envs 10 \
+    --num-minibatches 8 \
+    --vf-coef 10.0 \
+    --target-kl 0.01 \
+    --kl-loss True \
+    --seed "$seed"
+
+  echo "Experiment $run completed"
+  echo "----------------------------------------"
+done
