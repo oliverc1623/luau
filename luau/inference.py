a9597f6be9f728b0ad04cd4679a98e79a301c3c6
Oliver Chang <oliverc1622@gmail.com>
2025-06-13 12:34:31 -0700

    humanoid standup inference

--- luau/inference.py
+++ luau/inference.py
@@ -1,83 +1,110 @@
-# %%
+# %% Sac Protagonist Eval
 
-from pathlib import Path
+import math
 
 import gymnasium as gym
-import numpy as np
 import torch
-from minigrid.wrappers import ImgObsWrapper
-
-from luau.ppo import Agent
-
-
-root_path = Path(__file__).resolve().parent.parent
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-RGB_CHANNEL = 3
+import torch.nn.functional as f
+import wandb
+from torch import nn
 
 
 # %%
-def preprocess(image: np.array) -> dict:
-    """Preprocess the input for a grid-based environment, padding it to (12, 12, channels)."""
-    image = torch.from_numpy(image).float()
-    if image.ndim == RGB_CHANNEL:  # Single image case with shape (height, width, channels)
-        image = image.permute(2, 0, 1)
-        # Permute back to (batch_size, channels, height, width)
-        image = image.unsqueeze(0).to(device)  # Adding batch dimension
-    else:  # Batch case with shape (batch_size, height, width, channels)
-        image = image.permute(0, 3, 1, 2).to(device)  # Change to (batch, channels, height, width)
-    return image
+LOG_STD_MAX = 2
+LOG_STD_MIN = -5
+
+
+class Actor(nn.Module):
+    """A neural network that approximates the policy for reinforcement learning."""
+
+    def __init__(self, env: gym.Env, n_obs: int, n_act: int, device: str | None = None):
+        super().__init__()
+        self.fc1 = nn.Linear(n_obs, 256, device=device)
+        self.fc2 = nn.Linear(256, 256, device=device)
+        self.fc_mean = nn.Linear(256, n_act, device=device)
+        self.fc_logstd = nn.Linear(256, n_act, device=device)
+        # action rescaling
+        action_space = env.action_space
+        self.register_buffer(
+            "action_scale",
+            torch.tensor((action_space.high - action_space.low) / 2.0, dtype=torch.float32, device=device),
+        )
+        self.register_buffer(
+            "action_bias",
+            torch.tensor((action_space.high + action_space.low) / 2.0, dtype=torch.float32, device=device),
+        )
+
+    def forward(self, x: torch.tensor) -> tuple[torch.Tensor, torch.Tensor]:
+        """Forward pass."""
+        x = x.view(x.shape[0], -1)  # flatten the input
+        x = f.relu(self.fc1(x))
+        x = f.relu(self.fc2(x))
+        mean = self.fc_mean(x)
+        log_std = self.fc_logstd(x)
+        log_std = torch.tanh(log_std)
+        log_std = LOG_STD_MIN + 0.5 * (LOG_STD_MAX - LOG_STD_MIN) * (log_std + 1)  # From SpinUp / Denis Yarats
+
+        return mean, log_std
+
+    def get_action(self, x: torch.tensor) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+        """Get action from the policy network."""
+        mean, log_std = self(x)
+        std = log_std.exp()
+        normal = torch.distributions.Normal(mean, std)
+        x_t = normal.rsample()  # for reparameterization trick (mean + std * N(0,1))
+        y_t = torch.tanh(x_t)
+        action = y_t * self.action_scale + self.action_bias
+        log_prob = normal.log_prob(x_t)
+        # Enforcing Action Bound
+        log_prob -= torch.log(self.action_scale * (1 - y_t.pow(2)) + 1e-6)
+        log_prob = log_prob.sum(1, keepdim=True)
+        mean = torch.tanh(mean) * self.action_scale + self.action_bias
+        return action, log_prob, mean
 
 
 # %%
-seed = 1623
-door_locked = False
+seed = 1
 num_envs = 1
 
+env = gym.make("HumanoidStandup-v5")
 
-# env setup
-def make_env(subenv_seed: int) -> gym.Env:
-    """Create the environment."""
-
-    def _init() -> gym.Env:
-        env = gym.make("FourRoomDoorKeyLocked-v0", render_mode="rgb_array")
-        env = gym.wrappers.RecordEpisodeStatistics(env)
-        env = ImgObsWrapper(env)
-        env.reset(seed=subenv_seed)
-        env.action_space.seed(subenv_seed)
-        env.observation_space.seed(subenv_seed)
-        return env
-
-    return _init
-
-
-envs = [make_env(seed + i) for i in range(num_envs)]
-envs = gym.vector.SyncVectorEnv(envs)
+n_act = math.prod(env.action_space.shape)
+n_obs = math.prod(env.observation_space.shape)
+device = "cuda" if torch.cuda.is_available() else "cpu"
 
 # %%
-observation_shape = np.transpose(envs.single_observation_space.sample(), (2, 0, 1)).shape
-state_dim = envs.observation_space.shape[-1]
-# Initialize teacher model
-teacher_model_path = (
-    "../../../pvcvolume/runs/MiniGrid-DoorKey-6x6-v0__PPO_Teacher_Source__1733791056/"
-    "model/MiniGrid-DoorKey-6x6-v0__PPO_Teacher_Source__1733791056.pth"
+wandb.login(key="82555a3ad6bd991b8c4019a5a7a86f61388f6df1")
+wandb.init(
+    project="luau",
+    save_code=True,
 )
-teacher_source_agent = Agent(envs).to(device)
-teacher_source_agent.load_state_dict(torch.load(teacher_model_path))
-
 
 # %%
+best_model = wandb.restore("HumanoidStandup-v5__teacher__1__True__True_actor.pt", run_path="luau/sb0gwmg9")
 
-state, _ = envs.reset()
-done = False
-truncated = False
-step = 0
-while not done and not truncated:
-    state = preprocess(state)
-    action, _, _, _ = teacher_source_agent.get_action_and_value(state)
-    state, reward, done, truncated, _ = envs.step(action.cpu().numpy())
-    img = envs.render()
-print(reward)
-envs.close()
+# %%
+protagonist = Actor(env, device=device, n_act=n_act, n_obs=n_obs)
+protagonist.load_state_dict(
+    torch.load(best_model.name),
+)
+
+# %%
+try:
+    total_reward = 0
+    obs, _ = env.reset(seed=seed)
+    done = False
+    trunc = False
+    while not done and not trunc:
+        obs = torch.as_tensor(obs, device=device, dtype=torch.float).unsqueeze(0)
+        action, _, _ = protagonist.get_action(obs)
+        action = action.squeeze().detach().cpu().numpy()
+
+        obs, reward, done, trunc, info = env.step(action)
+        total_reward += reward
+    print(info)
+    print("episode_reward", total_reward)
+    total_reward = 0
+finally:
+    env.close()
 
 # %%
