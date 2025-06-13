e75c709aa0f864b0331d2705c8e41e7476f485f9
Oliver Chang <oliverc1622@gmail.com>
2025-06-12 17:18:33 -0700

    run script

---
+++ luau/run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Execute scripts with different seeds and additional arguments for torchcompile scripts
+scripts=(
+    sac_torchcompile.py
+)
+for script in "${scripts[@]}"; do
+    for seed in 11 21 31 41; do
+        if [[ $script == *_torchcompile.py ]]; then
+            python $script --seed=$seed \
+                --env-id "HumanoidStandup-v5" \
+                --exp_name "teacher" \
+                --num-envs 8 \
+                --gradient_steps -1 \
+                --cudagraphs \
+                --compile \
+        else
+            python $script --seed=$seed
+        fi
+    done
+done
