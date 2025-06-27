67a78688005fe4cb1760fccc650e003864d0fd35
Oliver Chang <oliverc1622@gmail.com>
2025-06-27 16:50:02 -0700

    curve

--- luau/driving-envs/run_baseline.sh
+++ luau/driving-envs/run_baseline.sh
@@ -7,13 +7,15 @@ for script in "${scripts[@]}"; do
     for seed in 11 21 31 42; do
         if [[ $script == *.py ]]; then
             python $script --seed=$seed \
-                --env-id "StraightRoad-v0" \
-                --exp_name "teacher" \
+                --env-id "CurveRoadDense-v0" \
+                --exp_name "student" \
                 --num-envs 8 \
                 --gradient_steps -1 \
                 --cudagraphs \
                 --compile \
-                --total-timesteps 1_000_000
+                --total-timesteps 1_000_000 \
+                --traffic_density 0.3 \
+                --map "C"
         else
             python $script --seed=$seed
         fi
