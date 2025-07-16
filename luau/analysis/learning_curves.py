716027801731eadb87e722a5583ee8118da2135e
Oliver Chang <oliverc1622@gmail.com>
2025-07-16 08:41:34 -0700

    learning curve generating code

---
+++ luau/analysis/learning_curves.py
@@ -0,0 +1,54 @@
+# %%
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+
+sns.set_theme(style="darkgrid")  # you can also pick 'darkgrid', 'white', etc.
+# Set font family to Times New Roman
+plt.rcParams["font.size"] = 34
+plt.rcParams["font.serif"] = ["Times New Roman"]
+plt.figure(figsize=(5, 4))
+
+# %%
+df = pd.read_csv("bipedal-learning-curves.csv")
+df = df.rename(
+    columns={
+        "Group: sac-baseline - episode_return": "Baseline",
+        "Group: sac-baseline - episode_return__MIN": "Baseline_Min",
+        "Group: sac-baseline - episode_return__MAX": "Baseline_Max",
+        "Group: sac-finetune - episode_return": "Finetune",
+        "Group: sac-finetune - episode_return__MIN": "Finetune_Min",
+        "Group: sac-finetune - episode_return__MAX": "Finetune_Max",
+        "Group: sac-iaa - episode_return": "IAA",
+        "Group: sac-iaa - episode_return__MIN": "IAA_Min",
+        "Group: sac-iaa - episode_return__MAX": "IAA_Max",
+        "Group: sac-diaa - episode_return": "DIAA",
+        "Group: sac-diaa - episode_return__MIN": "DIAA_Min",
+        "Group: sac-diaa - episode_return__MAX": "DIAA_Max",
+    },
+)
+
+# %%
+
+# Specify the algorithms you want to plot
+algorithms = ["Baseline", "Finetune", "IAA", "DIAA"]  # Extend as needed
+colors = sns.color_palette("Set2", 8)  # 8 discrete colors from "Set2"
+
+# Iterate over each algorithm
+for algo, color in zip(algorithms, colors, strict=False):
+    # Plot the main line
+    plt.plot(df["Step"], df[algo], color=color, label=algo)
+
+    # Shade the min and max
+    plt.fill_between(df["Step"], df[f"{algo}_Min"], df[f"{algo}_Max"], color=color, alpha=0.2)
+
+plt.xlabel("Step")
+plt.ylabel("Episodic Returns")
+plt.title("Two-lane Collision Avoidance Task")
+plt.tight_layout()
+plt.savefig("target_task_two_big_font.pdf", format="pdf")
+plt.show()
+
+# %%
