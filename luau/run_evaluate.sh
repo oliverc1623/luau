c7e4af4410f07bec2aa3a60470c882fe131631e8
Oliver Chang <oliverc1622@gmail.com>
2025-07-21 23:03:23 -0700

    bipedal run ids

--- luau/run_evaluate.sh
+++ luau/run_evaluate.sh
@@ -8,8 +8,8 @@ for script in "${scripts[@]}"; do
         if [[ $script == *.py ]]; then
             python $script --seed=$seed \
                 --env-id "BipedalWalker-v3" \
-                --student_run_id "akn48jga" \
-                --teacher_run_id "vmfqjyly" \
+                --student_run_id "kswwn4xa" \
+                --teacher_run_id "wxi10qyt" \
                 --env_kwargs hardcore True render_mode rgb_array
         else
             python $script --seed=$seed
