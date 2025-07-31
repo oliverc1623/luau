c3d031d4789be66fd2e17215debe4bcc7be8974b
Oliver Chang <oliverc1622@gmail.com>
2025-07-31 13:49:18 -0700

    add merge turn

--- luau/analysis/action_dist.py
+++ luau/analysis/action_dist.py
@@ -17,21 +17,25 @@ num_driving_actions = 2
 
 bdf = pd.read_csv("../BipedalWalker-v3_kswwn4xa_actions.csv")
 bdf = bdf[bdf["Action Dimension"] == "Action Dim 3"]
-bdf["env_id"] = "BipedalWalker-v3"
+bdf["env_id"] = "BWHM"
 
 ldf = pd.read_csv("../LunarLander-v3_akn48jga_actions.csv")
 ldf = ldf[ldf["Action Dimension"] == "Action Dim 2"]
-ldf["env_id"] = "LunarLander-v3"
+ldf["env_id"] = "LLWE"
 
 tdf = pd.read_csv("../driving-envs/TIntersection-v0_rfcac5sk_actions.csv")
-tdf["env_id"] = "TIntersection-v0"
+tdf["env_id"] = "T-Int."
 tdf = tdf[tdf["Action Dimension"] == "steering"]
 
 cydf = pd.read_csv("../driving-envs/CyCurveMerge-v0_wjbish1a_actions.csv")
-cydf["env_id"] = "CyCurveMerge-v0"
+cydf["env_id"] = "CM"
 cydf = cydf[cydf["Action Dimension"] == "throttle"]
 
-df_combined = pd.concat([tdf, cydf, bdf, ldf], ignore_index=True)
+yt = pd.read_csv("../driving-envs/MergeTurn-v0_2j49a0xn_actions.csv")
+yt["env_id"] = "MT"
+yt = yt[yt["Action Dimension"] == "steering"]
+
+df_combined = pd.concat([tdf, cydf, bdf, ldf, yt], ignore_index=True)
 
 # %%
 sns.set_theme(style="darkgrid")  # you can also pick 'darkgrid', 'white', etc.
@@ -53,13 +57,15 @@ g = sns.displot(
     palette={"Teacher": "#0A5FF5", "DIAA Student": "#f5a00a"},
     height=3,
     aspect=1.0,
-    col_wrap=2,
+    col_wrap=3,
 )
+sns.move_legend(g, "upper left", bbox_to_anchor=(0.55, 0.45))
 
-g.axes[0].text(-1.0, 0.3, "Steering Angle", fontsize=14)
-g.axes[1].text(-1.0, 0.3, "Driving Throttle", fontsize=14)
-g.axes[2].text(-1.0, 0.3, "Hip Throttle", fontsize=14)
-g.axes[3].text(-1.0, 0.3, "Lateral Throttle", fontsize=14)
+g.axes[0].text(-1.0, 0.24, "Steering Angle", fontsize=14)
+g.axes[1].text(-1.0, 0.24, "Driving Throttle", fontsize=14)
+g.axes[2].text(-1.0, 0.24, "Hip Throttle", fontsize=14)
+g.axes[3].text(-1.0, 0.24, "Lateral Throttle", fontsize=14)
+g.axes[4].text(-1.0, 0.24, "Steering Angle", fontsize=14)
 g.set_axis_labels("Action Value")
 g.set_titles(col_template="{col_name}")
 g.savefig("action_dist.pdf", dpi=300, bbox_inches="tight")
